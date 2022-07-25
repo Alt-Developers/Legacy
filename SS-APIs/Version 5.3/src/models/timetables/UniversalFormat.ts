6ecc8938772fac98e1abd8c8bc8d65db20090717
SEN-SENSATION <prawich.th@gmail.com>
2022-07-25 14:40:44 +0700

    SS APIs 5.3

---
+++ SS-APIs/Version 5.3/src/models/timetables/UniversalFormat.ts
@@ -0,0 +1,27 @@
+import { Schema, Types, connection, model } from "mongoose";
+import { TimetableInterface, UniversalCodeInterface } from "../types/modelType";
+
+const timetableSchema = new Schema(
+  {
+    school: {
+      type: String,
+      required: true,
+    },
+    universalCodes: {
+      EN: {
+        type: Object,
+      },
+      TH: {
+        type: Object,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const db = connection.useDb("timetables");
+
+export default db.model<UniversalCodeInterface>(
+  "UniversalFormat",
+  timetableSchema
+);
